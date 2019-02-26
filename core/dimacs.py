057fb64ae6981f4c708fcec35b052814800a7013
arijits17 <arijitshaw95@gmail.com>
2019-02-26 15:08:46 +0530

    Initial Commit

---
+++ core/dimacs.py
@@ -0,0 +1,16 @@
+
+
+
+
+# ---------------------------------------------
+# Function  :   parse
+# Input     :   DIMACS file for input SAT problem 
+#               and the Solver object
+# Output    :   None
+# Action    :   Clauses are put into the Solver object  
+# ---------------------------------------------
+
+
+def parse(S, filename):
+    # todo
+
---
+++ core/solver.py
@@ -0,0 +1,14 @@
+class Clause():
+
+
+
+class Solver():
+    def solve():
+
+
+    def addClause(lit p, lit q, lit r):
+
+
+    def simplify():
+
+    
