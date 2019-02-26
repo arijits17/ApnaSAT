df1b98a99a90e9357143874aaf3f15e45aa20169
Arijit Shaw <arijitshaw95@gmail.com>
2019-02-26 15:30:43 +0530

    Init

--- core/solver.py
+++ core/solver.py
@@ -3,12 +3,28 @@ class Clause():
 
 
 class Solver():
+
+    bool solved, conflict
+
+
     def solve():
+        search()
+
+    def search():
+        while(!solved || conflict):
+            addClause()
+            analyze()
 
 
     def addClause(lit p, lit q, lit r):
 
 
+
+    def analyze():
+
+        
     def simplify():
+
+
 
     
